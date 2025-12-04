4c83f983da558c6e8ced84966aa6a20ce13e5d79
samfreund <samf.236@proton.me>
2025-12-04 00:32:38 -0600

    day four

---
+++ 2025/d4/p1.java
@@ -0,0 +1,64 @@
+package d4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+class p1 {
+    public static void main(String[] args) {
+        try {
+            Scanner in = new Scanner(new File("2025/d4/input.txt"));
+
+            List<List<Boolean>> m = new ArrayList<List<Boolean>>();
+
+            while (in.hasNext()) {
+                m.add(List.of(in.nextLine().split("")).stream().map(c -> c.matches("@")).toList());
+            }
+
+            in.close();
+
+            int count = 0;
+
+            for (int x = 0; x < m.size(); x++) {
+                for (int y = 0; y < m.get(x).size(); y++) {
+                    if (m.get(x).get(y) && accessible(m, x, y, 4)) {
+                        count++;
+                    }
+                }
+            }
+
+            System.out.println(count);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * top left is origin
+     * if this is called, we expect the value at x,y is true
+     * 
+     * @param m matrix
+     * @param x x-coord
+     * @param y y-coord
+     * @param n number of allowable adjacent non-empty positions
+     * @return
+     */
+    private static boolean accessible(List<List<Boolean>> m, int x, int y, int n) {
+        int count = 0;
+
+        for (int x_1 = (x != 0 ? x - 1 : 0); x_1 <= ((x != m.size() - 1) ? x + 1 : m.size() - 1); x_1++) {
+            for (int y_1 = (y != 0 ? y - 1 : 0); y_1 <= ((y != m.get(x_1).size() - 1) ? y + 1
+                    : m.get(x_1).size() - 1); y_1++) {
+                if (!(x == x_1 && y == y_1) && m.get(x_1).get(y_1)) {
+                    count++;
+                }
+            }
+        }
+
+        return n > count;
+    }
+
+}
