2f880512a7f2563f7b9047ea9533a330bb5b21e2
samfreund <samf.236@proton.me>
2025-12-05 02:18:06 -0600

    d4p1 improvements

--- 2025/d4/p2.java
+++ 2025/d4/p2.java
@@ -1,10 +1,11 @@
 package d4;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.HashSet;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.Scanner;
+
 import utils.Tuple;
 
 class p2 {
@@ -12,10 +13,15 @@ class p2 {
         try {
             Scanner in = new Scanner(new File("2025/d4/input.txt"));
 
-            List<Boolean[]> m = new ArrayList<Boolean[]>();
+            HashSet<Tuple<Integer, Integer>> l = new HashSet<Tuple<Integer, Integer>>();
 
-            while (in.hasNext()) {
-                m.add(List.of(in.nextLine().split("")).stream().map(c -> c.matches("@")).toArray(Boolean[]::new));
+            for (int row = 0; in.hasNext(); row++) {
+                String s = in.nextLine();
+                for (int col = 0; col < s.length(); col++) {
+                    if (s.charAt(col) == '@') {
+                        l.add(new Tuple<Integer, Integer>(row, col));
+                    }
+                }
             }
 
             in.close();
@@ -25,21 +31,16 @@ class p2 {
 
             do {
                 iter_count = 0;
-                List<Tuple<Integer, Integer>> tuples = new ArrayList<Tuple<Integer, Integer>>();
-
-                for (int x = 0; x < m.size(); x++) {
-                    for (int y = 0; y < m.get(x).length; y++) {
-                        if (m.get(x)[y] && accessible(m, x, y, 4)) {
-                            iter_count++;
-                            tuples.add(new Tuple<Integer, Integer>(x, y));
-                        }
+                Iterator<Tuple<Integer, Integer>> it = l.iterator();
+                while (it.hasNext()) {
+                    Tuple<Integer, Integer> t = it.next();
+                    if (accessible(l, t, 4)) {
+                        iter_count++;
+                        it.remove();
                     }
-
                 }
 
                 total_count += iter_count;
-
-                tuples.stream().forEach(t -> m.get(t.x)[t.y] = false);
             } while (iter_count != 0);
 
             System.out.println(total_count);
@@ -53,19 +54,24 @@ class p2 {
      * top left is origin
      * if this is called, we expect the value at x,y is true
      * 
-     * @param m matrix
+     * @param l matrix
      * @param x x-coord
      * @param y y-coord
      * @param n number of allowable adjacent non-empty positions
      * @return
      */
-    private static boolean accessible(List<Boolean[]> m, int x, int y, int n) {
+    private static boolean accessible(HashSet<Tuple<Integer, Integer>> l, Tuple<Integer, Integer> t, int n) {
         int count = 0;
 
-        for (int x_1 = (x != 0 ? x - 1 : 0); x_1 <= ((x != m.size() - 1) ? x + 1 : m.size() - 1); x_1++) {
-            for (int y_1 = (y != 0 ? y - 1 : 0); y_1 <= ((y != m.get(x_1).length - 1) ? y + 1
-                    : m.get(x_1).length - 1); y_1++) {
-                if (!(x == x_1 && y == y_1) && m.get(x_1)[y_1]) {
+        for (int row = -1; row <= 1; row++) {
+            for (int col = -1; col <= 1; col++) {
+                if (row == 0 && col == 0) {
+                    continue;
+                }
+
+                Tuple<Integer, Integer> t_other = new Tuple<Integer, Integer>(t.x + row, t.y + col);
+
+                if (l.contains(t_other)) {
                     count++;
                 }
             }
