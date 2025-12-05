1b90ac7b68b19c1e817b896738302b3bdc7bb9cd
samfreund <samf.236@proton.me>
2025-12-05 02:17:53 -0600

    hash tuple

--- utils/Tuple.java
+++ utils/Tuple.java
@@ -1,15 +1,29 @@
 package utils;
 
+import java.util.Objects;
+
 public class Tuple<X, Y> {
     public final X x;
     public final Y y;
+    private final int hash;
 
     public Tuple(X x, Y y) {
         this.x = x;
         this.y = y;
+        this.hash = Objects.hash(this.x, this.y);
     }
 
-    public boolean equals(Tuple<X, Y> other) {
-        return this.x.equals(other.x) && this.y.equals(other.y);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (!(o instanceof Tuple)) return false;
+        Tuple<?, ?> other = (Tuple<?, ?>) o;
+        return Objects.equals(this.x, other.x) && Objects.equals(this.y, other.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return hash;
     }
 }
